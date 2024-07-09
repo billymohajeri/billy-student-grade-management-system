f5b2201156aae666e14c93f8677bb7a7e15af144
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-10 00:26:17 +0300

    Add display method

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,26 @@ public class Main {
   public static void main(String[] args) {
 
     Student firstStudent = new Student(1, "Billy", (short) 15);
-    System.out.println(firstStudent.id + ": " + firstStudent.name + " (" + firstStudent.age + " years old)");
+    firstStudent.displayInformation();
+//    System.out.println(firstStudent.id + ": " + firstStudent.name + " (" + firstStudent.age + " years old)");
   }
 
   static class Student {
-    int id;
-    String name;
-    short age;
+    private int id;
+    private String name;
+    private short age;
 
     Student(int id, String name, short age) {
       this.id = id;
       this.name = name;
       this.age = age;
-
     }
+
+    public void displayInformation() {
+      System.out.println(this.id + ": " + this.name + " (" + this.age + " years old)");
+    }
+
+
   }
 
 
